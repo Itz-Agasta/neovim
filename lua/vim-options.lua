78e27979976a6915c19ac1fd89859122c1a805df
Itz-Agasta <rupamgolui69@gmail.com>
2025-05-08 16:43:31 +0530

    updated the vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,13 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+-- Extras
+vim.wo.number = true -- Make line numbers default (default: false)
+vim.o.relativenumber = true -- Set relative numbered lines (default: false)
+vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
+
+vim.o.mouse = "a" -- Enable mouse mode (default: '')
+vim.o.autoindent = true -- Copy indent from current line when starting new one (default: true)
+vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
+vim.o.smartcase = true -- Smart case (default: false)
